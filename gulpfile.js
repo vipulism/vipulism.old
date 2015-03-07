b185d40b50467a6b6e549fc6dc11336086e9923f
Vipul <vipul0809@gmail.com>
2015-03-07 18:06:13 +0530

    css update

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,31 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify');
-	zip = require('gulp-zip');
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-ruby-sass');
+	// zip = require('gulp-zip');
+
 
 
 //scripts task 
 
 gulp.task('scripts', function () {	
-	gulp.src('js/*.js')
+	gulp.src('lib/js/*.js')
 	.pipe(uglify())
-	.pipe(gulp.dest('minjs'));
-})
+	.pipe(gulp.dest('js'));
+});
+
+gulp.task('style', function () {
+	
+	 return sass('lib/css/', ({style: 'compressed'}))
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+   })
+	.pipe(gulp.dest('css/'));
+});
 gulp.task('zip', function () {	
 		return gulp.src('js/*')
         .pipe(zip('archive.zip'))
         .pipe(gulp.dest('minjs'));
 
-})
+});
 
 gulp.task('default', ['scripts', 'style']);
